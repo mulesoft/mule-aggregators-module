77db2e50abdb006787d6e6f9f6ec36ac16911bca
Luciano RaineriMarchina <lucianoRM@users.noreply.github.com>
2018-07-16 18:25:04 -0300

    Mule 15371: Fix Aggregators UI (#25)

--- src/main/java/org/mule/extension/aggregator/internal/operations/AbstractAggregatorOperations.java
+++ src/main/java/org/mule/extension/aggregator/internal/operations/AbstractAggregatorOperations.java
@@ -7,10 +7,14 @@
 package org.mule.extension.aggregator.internal.operations;
 
 import static org.mule.runtime.api.meta.ExpressionSupport.NOT_SUPPORTED;
+import static org.mule.runtime.extension.api.annotation.param.display.Placement.ADVANCED_TAB;
 import org.mule.runtime.api.store.ObjectStore;
 import org.mule.runtime.extension.api.annotation.Expression;
 import org.mule.runtime.extension.api.annotation.param.Optional;
 import org.mule.runtime.extension.api.annotation.param.Parameter;
+import org.mule.runtime.extension.api.annotation.param.display.DisplayName;
+import org.mule.runtime.extension.api.annotation.param.display.Placement;
+import org.mule.runtime.extension.api.annotation.param.display.Summary;
 import org.mule.runtime.extension.api.annotation.param.reference.ObjectStoreReference;
 
 public abstract class AbstractAggregatorOperations {
@@ -19,6 +23,10 @@ public abstract class AbstractAggregatorOperations {
    * An ObjectStore for storing shared information regarding aggregators. (Groups, GroupId, Etc)
    */
   @Parameter
+  @Placement(tab = ADVANCED_TAB)
+  @DisplayName(value = "Object Store (*)")
+  @Summary(
+      value = "In order to create an Object Store configuration, an Object Store implementation needs to be present in the project. An option is to add the Mule Object Store Module provided by MuleSoft")
   @Expression(NOT_SUPPORTED)
   @Optional
   @ObjectStoreReference
