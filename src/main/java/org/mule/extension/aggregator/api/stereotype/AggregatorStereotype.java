72891cb5c86c322958413ec68c3843741ca29e94
Rodrigo Merino <elrodro83@users.noreply.github.com>
2019-07-24 10:58:13 -0300

    MULE-17242: Aggregators module does not have the stereotype on the (#46)
    * fix logging in AggregatorManager

---
+++ src/main/java/org/mule/extension/aggregator/api/stereotype/AggregatorStereotype.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.extension.aggregator.api.stereotype;
+
+import static org.mule.runtime.extension.api.stereotype.MuleStereotypes.PROCESSOR_DEFINITION;
+
+import org.mule.runtime.extension.api.stereotype.StereotypeDefinition;
+
+import java.util.Optional;
+
+/**
+ * {@link StereotypeDefinition} for a generic {@code AbstractAggregatorOperations}.
+ *
+ * @since 1.1, 1.0.2
+ */
+public class AggregatorStereotype implements StereotypeDefinition {
+
+  @Override
+  public String getName() {
+    return "AGGREGATOR";
+  }
+
+  @Override
+  public Optional<StereotypeDefinition> getParent() {
+    return Optional.of(PROCESSOR_DEFINITION);
+  }
+
+}
