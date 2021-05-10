a1c074d1bf976b863049d7e597b7f91ceb73913f
Ignacio Giss <rgiss@mulesoft.com>
2021-05-10 17:25:25 -0300

    AMOD-9: Update messages pending aggregation in ObjectStore when upgrading to 1.0.3 in group-based aggregators (#106)

--- src/main/java/org/mule/extension/aggregator/internal/storage/info/GroupAggregatorSharedInformation.java
+++ src/main/java/org/mule/extension/aggregator/internal/storage/info/GroupAggregatorSharedInformation.java
@@ -12,6 +12,7 @@ import org.mule.extension.aggregator.internal.task.AsyncTask;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class GroupAggregatorSharedInformation implements AggregatorSharedInformation {
 
@@ -68,9 +69,18 @@ public class GroupAggregatorSharedInformation implements AggregatorSharedInforma
   /**
    * This method upgrades the sequenced elements to the new data structure for backward compatibility.
    * It is not necessary to do an upgrade for this class.
+   *
+   * @return true if any upgrade was made, false otherwise.
    */
+  @Deprecated
   @Override
   public boolean upgradeIfNeeded() {
-    return false;
+    boolean hasChanges = false;
+    for (AggregatedContent aggregatedContent : contentMap.values()) {
+      if ((!Objects.isNull(aggregatedContent))) {
+        hasChanges = hasChanges || aggregatedContent.upgradeIfNeeded();
+      }
+    }
+    return hasChanges;
   }
 }
